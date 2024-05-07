a9a8c8db867c61de4f98a7e6b96254bec437706d
nmBPLG4413wQ <horpynych_ap@groupbwt.com>
2024-05-07 23:31:40 +0300

    feat: configurations for project

---
+++ .eslintrc.js
@@ -0,0 +1,23 @@
+module.exports = {
+  extends: ['eslint:recommended', 'plugin:prettier/recommended', 'prettier'],
+  plugins: ['prettier', 'import'],
+  parser: '@babel/eslint-parser',
+
+  env: {
+    browser: true,
+    node: true,
+    es6: true,
+  },
+  rules: {
+    quotes: [2, 'single', { avoidEscape: true }],
+    'no-mixed-spaces-and-tabs': 0,
+    'no-sequences': [0, 'SequenceExpression'],
+    'prettier/prettier': 'error',
+    'no-duplicate-imports': [
+      'error',
+      {
+        includeExports: true,
+      },
+    ],
+  },
+};
