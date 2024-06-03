efcc59e03a6c508200e579508c42e72e223a4e64
nmBPLG4413wQ <horpynych_ap@groupbwt.com>
2024-06-03 15:16:31 +0300

    fix: dto for commission manager

--- dist/services/commissions/commissionManager.js
+++ dist/services/commissions/commissionManager.js
@@ -6,23 +6,40 @@ const cashInNaturalService_1 = require("./cashInNaturalService");
 const cashOutNaturalService_1 = require("./cashOutNaturalService");
 const cashOutJuridicalService_1 = require("./cashOutJuridicalService");
 class CommissionManager {
+    // private strategies: { [key: string]: any };
     constructor(transfer) {
         this.transfer = transfer;
-        this.strategies = {
-            [constants_1.COMMISION_TYPE.cashInNatural]: new cashInNaturalService_1.CashInNaturalService(transfer),
-            [constants_1.COMMISION_TYPE.cashInJuridical]: new cashInNaturalService_1.CashInNaturalService(transfer),
-            [constants_1.COMMISION_TYPE.cashOutNatural]: new cashOutNaturalService_1.CashOutNaturalService(transfer),
-            [constants_1.COMMISION_TYPE.cashOutJuridical]: new cashOutJuridicalService_1.CashOutJuridicalService(transfer),
+        // this.strategies = {
+        //   [COMMISION_TYPE.cashInNatural]: new CashInNaturalService(transfer),
+        //   [COMMISION_TYPE.cashInJuridical]: new CashInNaturalService(transfer),
+        //   [COMMISION_TYPE.cashOutNatural]: new CashOutNaturalService(transfer),
+        //   [COMMISION_TYPE.cashOutJuridical]: new CashOutJuridicalService(transfer),
+        // };
+    }
+    getStrategy() {
+        const operationType = constants_1.OPERATION_TYPE[this.transfer.type];
+        const userType = operationType[this.transfer.user_type];
+        const strategyMap = {
+            [constants_1.COMMISION_TYPE.cashInNatural]: cashInNaturalService_1.CashInNaturalService,
+            [constants_1.COMMISION_TYPE.cashInJuridical]: cashInNaturalService_1.CashInNaturalService, // Assuming this is correct, otherwise update accordingly
+            [constants_1.COMMISION_TYPE.cashOutNatural]: cashOutNaturalService_1.CashOutNaturalService,
+            [constants_1.COMMISION_TYPE.cashOutJuridical]: cashOutJuridicalService_1.CashOutJuridicalService,
         };
+        return strategyMap[userType];
     }
     calculate() {
         if (this.transfer.operation.currency !== 'EUR') {
             return { error: 'Only supported currency is EUR' };
         }
-        const operationType = constants_1.OPERATION_TYPE[this.transfer.type];
-        const userType = operationType[this.transfer.user_type];
-        const situation = this.strategies[userType];
-        return { commission: situation.getCommission() };
+        // const operationType = OPERATION_TYPE[this.transfer.type];
+        // const userType = operationType[this.transfer.user_type];
+        // const situation = this.strategies[userType];
+        const StrategyClass = this.getStrategy();
+        if (!StrategyClass) {
+            return { error: 'Unsupported operation type or user type' };
+        }
+        const strategyInstance = new StrategyClass(this.transfer);
+        return { commission: strategyInstance.getCommission() };
     }
 }
 exports.CommissionManager = CommissionManager;
--- src/services/commissions/commissionManager.ts
+++ src/services/commissions/commissionManager.ts
@@ -5,27 +5,44 @@ import { CashOutJuridicalService } from './cashOutJuridicalService';
 import { inputDataDto } from '../../utils/parseJsonFile';
 import { commissionDto } from 'src/modules/calculateUserCommissionModule';
 
+type StrategyClass = 
+  | typeof CashInNaturalService
+  | typeof CashOutNaturalService
+  | typeof CashOutJuridicalService;
+
 export class CommissionManager {
-  private transfer: inputDataDto
-  private strategies: { [key: string]: any };
+  private transfer: inputDataDto;
 
   constructor(transfer: inputDataDto) {
     this.transfer = transfer;
-    this.strategies = {
-      [COMMISION_TYPE.cashInNatural]: new CashInNaturalService(transfer),
-      [COMMISION_TYPE.cashInJuridical]: new CashInNaturalService(transfer),
-      [COMMISION_TYPE.cashOutNatural]: new CashOutNaturalService(transfer),
-      [COMMISION_TYPE.cashOutJuridical]: new CashOutJuridicalService(transfer),
+  }
+
+  private getStrategy(): StrategyClass | undefined {
+    const operationType = OPERATION_TYPE[this.transfer.type];
+    const userType = operationType[this.transfer.user_type];
+
+    const strategyMap = {
+      [COMMISION_TYPE.cashInNatural]: CashInNaturalService,
+      [COMMISION_TYPE.cashInJuridical]: CashInNaturalService,
+      [COMMISION_TYPE.cashOutNatural]: CashOutNaturalService,
+      [COMMISION_TYPE.cashOutJuridical]: CashOutJuridicalService,
     };
+
+    return strategyMap[userType];
   }
 
   calculate(): commissionDto {
     if (this.transfer.operation.currency !== 'EUR') {
       return { error: 'Only supported currency is EUR' };
     }
-    const operationType = OPERATION_TYPE[this.transfer.type];
-    const userType = operationType[this.transfer.user_type];
-    const situation = this.strategies[userType];
-    return { commission: situation.getCommission() };
+
+    const StrategyClass = this.getStrategy();
+
+    if (!StrategyClass) {
+      return { error: 'Unsupported operation type or user type' };
+    }
+
+    const strategyInstance = new StrategyClass(this.transfer);
+    return { commission: strategyInstance.getCommission() };
   }
 }
